b73aea7ebe4e2562d7ca217d7009a84547b67791
Giovanne Mendonça <giovannemendonca@outlook.com>
2024-02-11 21:05:06 -0300

    initial project

---
+++ src/test/java/com/mendonca/voll_med/VollMedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mendonca.voll_med;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VollMedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
